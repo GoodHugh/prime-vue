e9fa87dee79a4e90964de20f0912e277bdd2f231
Nazari <nazariadamenko@gmail.com>
2024-10-14 15:45:01 +0300

    main - init project

---
+++ nuxt.config.ts
@@ -0,0 +1,34 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  srcDir: '.',
+
+  dir: {
+    app: 'app'
+  },
+
+  experimental: {
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    normalizeComponentNames: false,
+    defaults: {
+      useAsyncData: {
+        deep: true
+      }
+    }
+  },
+
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false
+    }
+  }
+})
