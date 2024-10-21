cc9cdd2e50b89cecd78769d249f60db5eb372621
Nazari <nazariadamenko@gmail.com>
2024-10-21 17:24:38 +0300

    setting-prime-vue - настройка семантических токенов, цветовой схемы, настройка темного режима и кастомизация палитры цветов

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts" >
 const text = ref('');
+
+function toggleDarkMode() {
+  document.documentElement.classList.toggle('my-app-dark');
+}
 </script>
 
 <template>
-  <div>
-    <span> Hello prime vue </span>
-    <InputText v-model="text" />
-    <Button> Save </Button>
+  <div class="my-card">
+    <InputText v-model="text" style="margin-right: 1rem" />
+    <Button label="Save" />
+    <br>
+    {{ text }}
+    <br>
+    <Button label="Toggle dark mode" @click="toggleDarkMode"/>
   </div>
 </template>
