cd7bd24e2a37c032031ae75eefa376d0f010e932
Nazari <nazariadamenko@gmail.com>
2024-10-22 11:30:13 +0300

    updating-color-runtime - изучение функционала измененя палтны в реальном времени

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts" >
+import {updatePrimaryPalette, updateSurfacePalette} from "@primeuix/styled";
+
 const text = ref('');
 const value = ref(null);
 
@@ -35,6 +37,57 @@ const cities = ref([
 function toggleDarkMode() {
   document.documentElement.classList.toggle('my-app-dark');
 }
+
+function greenPrimary() {
+  updatePrimaryPalette({
+    50: '{green.50}',
+    100: '{green.100}',
+    200: '{green.200}',
+    300: '{green.300}',
+    400: '{green.400}',
+    500: '{green.500}',
+    600: '{green.600}',
+    700: '{green.700}',
+    800: '{green.800}',
+    900: '{green.900}',
+    950: '{green.950}'
+  })
+}
+
+function purplePrimary() {
+  updatePrimaryPalette({
+    50: '{purple.50}',
+    100: '{purple.100}',
+    200: '{purple.200}',
+    300: '{purple.300}',
+    400: '{purple.400}',
+    500: '{purple.500}',
+    600: '{purple.600}',
+    700: '{purple.700}',
+    800: '{purple.800}',
+    900: '{purple.900}',
+    950: '{purple.950}'
+  })
+}
+
+function changeSurface() {
+  updateSurfacePalette({
+    dark: {
+      50: '{slate.50}',
+      100: '{slate.100}',
+      200: '{slate.200}',
+      300: '{slate.300}',
+      400: '{slate.400}',
+      500: '{slate.500}',
+      600: '{slate.600}',
+      700: '{slate.700}',
+      800: '{slate.800}',
+      900: '{slate.900}',
+      950: '{slate.950}'
+    }
+  })
+}
+
 </script>
 
 <template>
@@ -45,6 +98,15 @@ function toggleDarkMode() {
     {{ text }}
     <br>
     <Button label="Toggle dark mode" @click="toggleDarkMode"/>
+    <br>
+    <br>
+    <Button label="Green" @click="greenPrimary" severity="success"/>
+    <br>
+    <br>
+    <Button label="Purple" @click="purplePrimary" severity="help"/>
+    <br>
+    <br>
+    <Button label="Slate" @click="changeSurface" severity="secondary"/>
   </div>
 
   <div class="card flex justify-center">
